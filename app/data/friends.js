21e1dd734266e1c80d22a28df6ce5271224b433c
Jayrene1 <43413926+Jayrene1@users.noreply.github.com>
2019-02-23 17:48:15 -0500

    initial site files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,63 @@
 var friends = [
   {
-    name: "Jim",
+    name: "Jim Halpert",
     photo:
-      "https://media.giphy.com/media/hyMFaxhuQkZTq/giphy.gif",
-    scores: [1, 2, 4, 4, 5, 4, 3, 2, 4, 2]
+      "https://media.giphy.com/media/BUQKKSex1oc8M/giphy.gif",
+    scores: [3, 3, 3, 2, 2, 5, 5, 3, 3, 5]
   },
   {
-    name: "Pam",
+    name: "Pam Beesly",
     photo:
-      "https://media.giphy.com/media/hyMFaxhuQkZTq/giphy.gif",
-    scores: [4, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      "https://media.giphy.com/media/3oEjHYibHwRL7mrNyo/giphy.gif",
+    scores: [2, 1, 4, 4, 2, 3, 5, 5, 4, 4]
   },
   {
-    name: "Dwight",
+    name: "Dwight Schrute",
+    photo:
+      "https://media.giphy.com/media/4wXguZCzYopRC/giphy.gif",
+    scores: [1, 5, 5, 1, 1, 3, 4, 1, 3, 2]
+  },
+  {
+    name: "Michael Scott",
     photo:
       "https://media.giphy.com/media/hyMFaxhuQkZTq/giphy.gif",
-    scores: [1, 1, 4, 5, 1, 1, 2, 3, 1, 5]
+    scores: [2, 2, 3, 5, 5, 5, 5, 3, 5, 3]
+  },
+  {
+    name: "Andy Bernard",
+    photo:
+      "https://media.giphy.com/media/l3Ucl5pIqSaGa82T6/giphy.gif",
+    scores: [4, 1, 5, 4, 4, 4, 4, 5, 2, 4]
+  },
+  {
+    name: "Jan Levinson",
+    photo:
+      "https://media.giphy.com/media/nFD6gUOXkk1Ko/giphy.gif",
+    scores: [1, 1, 2, 2, 2, 5, 2, 3, 1, 4]
+  },
+  {
+    name: "Stanley Hudson",
+    photo:
+      "https://media.giphy.com/media/dEdmW17JnZhiU/giphy.gif",
+    scores: [2, 2, 2, 2, 2, 1, 1, 2, 2, 3]
+  },
+  {
+    name: "Angela Martin",
+    photo:
+      "https://media.giphy.com/media/LwMdZM4vIJPnG/giphy.gif",
+    scores: [5, 1, 4, 3, 2, 4, 5, 1, 5, 5]
+  },
+  {
+    name: "Kelly Kapoor",
+    photo:
+      "https://media.giphy.com/media/P43lFJyUBMBna/giphy.gif",
+    scores: [2, 4, 5, 5, 5, 2, 3, 4, 2, 3]
+  },
+  {
+    name: "Ryan Howard",
+    photo:
+      "https://media.giphy.com/media/CuNDBADClcdVe/giphy.gif",
+    scores: [3, 3, 4, 5, 4, 4, 2, 3, 2, 4]
   }
 ];
 
